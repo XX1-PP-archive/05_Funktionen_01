2986f3f73149fadf3e749aa0cbf40020f1a84c10
Peter <schmidt.multimedia@gmail.com>
2021-05-20 09:32:00 +0200

    Funktionen 01

--- script_01.js
+++ script_01.js
@@ -2,3 +2,13 @@
 /***** Funktionen 01 *****/
 
 // 1. Kapselung von Codeblöcken
+
+// Funktionsrumpf (body)
+function test()
+{
+    console.log("Hallo Deniz!");
+}
+
+// Funktionsaufruf (call)
+//test();
+
