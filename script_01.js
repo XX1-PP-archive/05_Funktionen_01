ae22cb451bdfa672e2f497b6199c67eae7f08aa7
mwcomputer <kolbenfresser5@googlemail.com>
2021-05-20 10:04:53 +0200

    schritt 1

--- script_01.js
+++ script_01.js
@@ -2,3 +2,51 @@
 /***** Funktionen 01 *****/
 
 // 1. Kapselung von Codeblöcken
+
+//Funktionsrumpf (body)
+function test()
+{
+    console.log("Hallo Deniz");    
+}
+
+// Finktionsaufruf (call)
+//test();
+
+/***** Funktionen 02a******/
+// 2a. Paramentiesierung + Datenrückgabe von INNEN
+
+ function ausgabeName()
+  {
+    let firstname = "Deniz";  
+    console.log("Hallo " + firstname + "!");
+  }
+
+ausgabeName();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
